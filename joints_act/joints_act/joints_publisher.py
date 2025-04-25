85fc6c2a7a5ac50efdcb95745cf141fba7eeba02
Sergio Corona <sergio_corona08@hotmail.com>
2025-04-24 20:32:41 -0600

    Add files via upload

--- joints_act/joints_act/joints_publisher.py
+++ joints_act/joints_act/joints_publisher.py
@@ -1,6 +1,9 @@
 import rclpy
 from rclpy.node import Node
+from tf2_ros import TransformBroadcaster
+from geometry_msgs.msg import TransformStamped
 from sensor_msgs.msg import JointState
+import transforms3d
 import numpy as np
 
 class joints_publisher(Node):
@@ -23,20 +26,67 @@ class joints_publisher(Node):
         self.ctrlJoints.velocity = [1.0, 1.0]  # Dos velocidades
         self.ctrlJoints.effort = [0.0, 0.0]  # Dos esfuerzos
 
-        # Initialise Variables
-        self.i = 0.0
-        self.sign = 1
+        #Drone Initial Pose
+        self.intial_pos_x = 1.0
+        self.intial_pos_y = 1.0
+        self.intial_pos_z = 0.0
+        self.intial_pos_yaw = np.pi/2
+        self.intial_pos_pitch = 0.0
+        self.intial_pos_roll = 0.0
+
+        #Angular velocity for the pose change and propellers
+        self.omega = 0.5
+        self.omega_prop = 50
+
+        #Define Transformations
+        self.define_TF()
+
+        #Create Transform Boradcasters
+        self.tf_br_base = TransformBroadcaster(self)
+        self.tf_br_base_footprint = TransformBroadcaster(self)
+
 
     # Timer Callback
     def timer_cb(self):
+
         time = self.get_clock().now().nanoseconds / 1e9
+
+        #TRASLACION COMPLETA DEL ROBOT
+        #Create Trasnform Messages
+        self.base_link_tf.header.stamp = self.get_clock().now().to_msg()
+        self.base_link_tf.transform.translation.x = self.intial_pos_x + 0.5*np.cos(self.omega*time) #MOVIMIENTO DE TRASLACION EN X
+        self.base_link_tf.transform.translation.y = self.intial_pos_y + 0.5*np.sin(self.omega*time) #MOVIMIENTO DE TRASLACION EN Y
+        self.base_link_tf.transform.translation.z = self.intial_pos_z    #MOVIMIENTO DE TRASLACION EN Z, EN ESTE CASO SE MANTIENE
+        q = transforms3d.euler.euler2quat(self.intial_pos_roll, self.intial_pos_pitch, self.intial_pos_yaw+self.omega*time)       
+        self.base_link_tf.transform.rotation.x = q[1]
+        self.base_link_tf.transform.rotation.y = q[2]
+        self.base_link_tf.transform.rotation.z = q[3]
+        self.base_link_tf.transform.rotation.w = q[0]
 
         self.ctrlJoints.header.stamp = self.get_clock().now().to_msg()
         self.ctrlJoints.position[0] = np.sin(time)
-        self.ctrlJoints.position[1] = np.cos(time)  
+        self.ctrlJoints.position[1] = np.cos(time)  #ESTO SIMPLEMENTE MUEVE LAS RUEDAS
+
+        self.tf_br_base.sendTransform(self.base_link_tf)
 
         self.publisher.publish(self.ctrlJoints)
 
+    def define_TF(self):
+
+        #Create Trasnform Messages
+        self.base_link_tf = TransformStamped()
+        self.base_link_tf.header.stamp = self.get_clock().now().to_msg()
+        self.base_link_tf.header.frame_id = 'odom'
+        self.base_link_tf.child_frame_id = 'base_link'
+        self.base_link_tf.transform.translation.x = self.intial_pos_x
+        self.base_link_tf.transform.translation.y = self.intial_pos_y
+        self.base_link_tf.transform.translation.z = self.intial_pos_z
+        q = transforms3d.euler.euler2quat(self.intial_pos_roll, self.intial_pos_pitch, self.intial_pos_yaw)       
+        self.base_link_tf.transform.rotation.x = q[1]
+        self.base_link_tf.transform.rotation.y = q[2]
+        self.base_link_tf.transform.rotation.z = q[3]
+        self.base_link_tf.transform.rotation.w = q[0]
+ 
 def main(args=None):
     rclpy.init(args=args)
     node = joints_publisher()
--- joints_act/launch/fixed_launch.py
+++ joints_act/launch/fixed_launch.py
@@ -3,6 +3,11 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+from launch.actions import  EmitEvent, LogInfo, RegisterEventHandler
+from launch.event_handlers import OnProcessExit, OnShutdown
+from launch.events import Shutdown
+from launch.substitutions import EnvironmentVariable, LocalSubstitution
+
 def generate_launch_description():
 #Todo EL PATH A DONDE ESTA LA UBICACION
     urdf_file_name = 'fixed_ex.urdf'
@@ -16,6 +21,21 @@ def generate_launch_description():
         robot_desc = infp.read()
 
 
+    static_transform_node = Node(
+                                package='tf2_ros',
+                                executable='static_transform_publisher',
+                                arguments = ['--x', '2', '--y', '1', '--z', '0.0',
+                                            '--yaw', '0.0', '--pitch', '0', '--roll', '0.0',
+                                            '--frame-id', 'map', '--child-frame-id', 'odom']
+                                )
+
+    static_transform_node_2 = Node(
+                                package='tf2_ros',
+                                executable='static_transform_publisher',
+                                arguments = ['--x', '0', '--y', '0', '--z', '0.0',
+                                            '--yaw', '0.0', '--pitch', '0', '--roll', '0.0',
+                                            '--frame-id', 'world', '--child-frame-id', 'map']
+                                )
     robot_state_pub_node = Node(
                             package='robot_state_publisher',
                             executable='robot_state_publisher',
@@ -32,10 +52,57 @@ def generate_launch_description():
                             executable='joints_publisher',
                             output='screen',
                             )
+    rviz_config = os.path.join(
+                            get_package_share_directory('link_act'),
+                            'rviz',
+                            'puzzledrone.rviz'
+                            )
+    
+    rviz_node = Node(name='rviz',
+                    package='rviz2',
+                    executable='rviz2',
+                    arguments=['-d', rviz_config],
+                    on_exit=Shutdown(),
+                    )
+    
+    rqt_tf_tree_node = Node(name='rqt_tf_tree',
+                    package='rqt_tf_tree',
+                    executable='rqt_tf_tree'
+                    )    
+
+    shutdown_on_exit = [RegisterEventHandler(
+                            OnProcessExit(
+                                target_action=node,
+                                on_exit=[
+                                    LogInfo(msg=(EnvironmentVariable(name='USER'),
+                                            ' exited the node')),
+                                    EmitEvent(event=Shutdown(
+                                        reason='Node Exited'))
+                                ]
+                            )
+                        ) for node in [robot_state_pub_node, Mydynamic_transform_node]
+                    ]
+ 
+
+    # Ensure full shutdown when SIGINT (Ctrl+C) is received
+    shutdown_log = RegisterEventHandler(
+                                    OnShutdown(
+                                        on_shutdown=[LogInfo(
+                                            msg=['Launch was asked to shutdown: ',
+                                                LocalSubstitution('event.reason')]
+                                        )]
+                                    )
+                                )
 
 
-    l_d = LaunchDescription([
-                              robot_state_pub_node,
-                              Mydynamic_transform_node])
+
+    l_d = LaunchDescription([   static_transform_node, 
+                                static_transform_node_2,
+                                robot_state_pub_node,
+                                Mydynamic_transform_node, 
+                                rviz_node, 
+                                rqt_tf_tree_node,  
+                                shutdown_log, 
+                                *shutdown_on_exit])
 
     return l_d
